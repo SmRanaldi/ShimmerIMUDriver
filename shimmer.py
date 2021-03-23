Add axis-angle representation. Fixed gyro units

shimmer.py
@@ -12,6 +12,7 @@ class Shimmer:
     # ----- Parameters -----
     baud_rate = 115200
     length_calibration_packet = 21
+    NORM_THS = 1e-6
 
     sensors_list = {
         # [0] - On command.
@@ -287,16 +288,18 @@ class Shimmer:
 
     def get_orientation(self, a, g, m, q):
 
-        beta = 0.01
+        beta = 0.1
         sampling_period = 1/self.sampling_rate
         quaternion_norm = lambda x: np.sqrt(np.sum([c**2 for c in quaternion.as_float_array(x)])) 
 
         norm_a = quaternion_norm(a)
-        if norm_a > 1e-6: a /= norm_a
+        if norm_a > self.NORM_THS: a /= norm_a
         norm_m = quaternion_norm(m)
-        if norm_m > 1e-6: a /= norm_m
+        if norm_m > self.NORM_THS: a /= norm_m
         norm_q = quaternion_norm(q)
-        if norm_q > 1e-6: a /= norm_q
+        if norm_q > self.NORM_THS: a /= norm_q
+
+        g /= (180/np.pi) # Get gyro data in radians/s
 
         ht = quaternion.as_float_array(q * m * q.conj())
         bt = np.quaternion(0, np.sqrt(ht[1]**2 + ht[2]**2), 0, ht[3])
@@ -326,7 +329,7 @@ class Shimmer:
         f = Jgb.transpose()@fgb
         f = np.quaternion(*f)
         norm_f = quaternion_norm(f)
-        if norm_f > 1e-6: f /= norm_f
+        if norm_f > self.NORM_THS: f /= norm_f
 
         # Eq. 30 in the paper
         q_dot = 0.5 * q * g - beta*f
@@ -347,10 +350,15 @@ class Shimmer:
             angles_out.append(quaternion.as_euler_angles(q))
         return np.array(angles_out)
 
-    def get_rotation_axis(self):
+    def get_axis_angle(self):
         quat = self.get_quaternions()
         axis_out = []
+        angle_out = []
         for q in quat:
             Q = quaternion.as_float_array(q)
+            row = []
             axis_out.append(Q[1:]/np.sqrt(1-Q[0]**2))
-        return np.array(axis_out)
+            angle_out.append(2*np.arccos(Q[0]))
+        angle_out = np.array(angle_out)
+        axis_out = np.array(axis_out)
+        return np.concatenate((axis_out,angle_out.reshape(-1,1)), axis=1)

test_class.py
@@ -53,7 +53,7 @@ for s in shimmers:
     l.append(data[s.com_port]['SENSOR_A_ACCEL'].shape[0])
     plt.subplot(121)
     for i in range(0,3):
-        plt.plot(data[s.com_port]['SENSOR_A_ACCEL'][:,i])
+        plt.plot(data[s.com_port]['SENSOR_MPU9150_GYRO'][:,i])
     plt.subplot(122)
     for i in range(0,3):
         plt.plot(data[s.com_port]['SENSOR_LSM303DLHC_MAG'][:,i])
@@ -62,21 +62,22 @@ min_l = np.min(l)
 
 plt.figure()
 for s in shimmers:
-    plt.plot(data[s.com_port]['SENSOR_A_ACCEL'][:,0], label = s.com_port)
+    plt.plot(data[s.com_port]['SENSOR_MPU9150_GYRO'][:,0], label = s.com_port)
 plt.legend()
 plt.show()
 
 for s in shimmers:
-    # ang = s.get_euler_angles()
-    # ang *= 180/np.pi
-    r_axis = s.get_rotation_axis()
+    ang = s.get_euler_angles()
+    ang *= 180/np.pi
+    r_axis = s.get_axis_angle()
+    print(r_axis.shape)
     fig = plt.figure()
-    fig.add_subplot(111, projection='3d')
-    # for i in range(3):
-    #     r_axis[:,i] = np.convolve(r_axis[:,i], np.ones(50)/50, mode='same')
+    fig.add_subplot(121, projection='3d')
     # plt.plot(ang)
     plt.plot(r_axis[:,0],r_axis[:,1],r_axis[:,2],'ko')
     plt.plot(0,0,0,'ro',markersize=5)
+    fig.add_subplot(122)
+    plt.plot(r_axis[:,3],'k')
     plt.show()
 for s in shimmers:
     s.disconnect()